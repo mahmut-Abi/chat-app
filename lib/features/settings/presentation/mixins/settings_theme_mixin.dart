58c105dd1fd3e2d646fedfa4cc9543e80703bd96
maihemutimaimaiti <maihemutimaimaiti@360.cn>
2025-10-15 11:29:08 +0800

    fix(设置): 移除未使用的 go_router 导入

--- lib/features/settings/presentation/mixins/settings_theme_mixin.dart
+++ lib/features/settings/presentation/mixins/settings_theme_mixin.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import '../../../../core/providers/providers.dart';
 import '../../../../shared/themes/app_theme.dart';
 import '../background_settings_screen.dart';
